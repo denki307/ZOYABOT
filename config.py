b3de8c95e72289388982b2e859c86c20a8f288ca
denki307 <mbtdenki@gmail.com>
2025-03-07 06:48:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -68,7 +68,7 @@ GIT_TOKEN = getenv(
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MUSIC_BOT_TEAM")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FANTASTIC_FAMILY_TAMIL")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PURPLE_HEARTS_GROUP")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
